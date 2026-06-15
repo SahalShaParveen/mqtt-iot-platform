2983cc623eee14ed9ef211d9605c7d22fddde92d
SahalShaParveen <sahalshaparveen@gmail.com>
2026-06-15 16:46:20 +0000

    Add pi metrics to app.py, and deal with empty rows

--- raspberry-pi/app.py
+++ raspberry-pi/app.py
@@ -10,7 +10,7 @@ def get_latest_metric(metric_name, device_name):
     cursor = conn.cursor()
 
     cursor.execute("""
-    SELECT timestamp, value
+    SELECT value
     FROM readings
     WHERE metric = ? AND device=?
     ORDER BY id DESC
@@ -20,22 +20,29 @@ def get_latest_metric(metric_name, device_name):
     row = cursor.fetchone()
     conn.close()
 
-    return row
+    value = row[0] if row is not None else None
+    return value
 
 
 @app.route("/")
 def home():
-    temperature_info = get_latest_metric("temperature", "esp32_1")
-    humidity_info = get_latest_metric("humidity", "esp32_1")
-
-    temperature = temperature_info[1]
-    humidity = humidity_info[1]
+    temp = get_latest_metric("temperature", "esp32_1")
+    humidity = get_latest_metric("humidity", "esp32_1")
+    cpu_temp = get_latest_metric("cpu_temp", "pi")
+    ram_usage = get_latest_metric("ram_usage", "pi")
+    disk_usage = get_latest_metric("disk_usage", "pi")
 
     return f"""
         <h2>ESP32_1</h2>
 
-        <p>Temperature: {temperature} °C</p>
+        <p>Temperature: {temp} °C</p>
         <p>Humidity: {humidity} %</p>
+        
+        <h2>Raspberry Pi</h2>
+        <p>CPU Temperature: {cpu_temp} °C</p>
+        <p>RAM Usage : {ram_usage}%</p>
+        <p>Disk Usage : {disk_usage}%</p>
+    
     """
 
 
